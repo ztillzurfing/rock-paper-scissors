d0abede009f5411097d3c3d7569bf6c6b17add78
ztillzurfing <94315453+ztillzurfing@users.noreply.github.com>
2024-03-27 13:04:04 +0000

    add getComputerChoice( "Rock","Paper","Scissors")
    Created a function that generates a random number (0->1)*3 then rounds
    down and assigned the game choices

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+
+function getComputerChoice() {
+
+    let choice = Math.floor(Math.random()*3);
+
+    if (choice == 0) {
+        return ("Rock");
+    } else if (choice == 1) {
+        return ("Paper");
+    } else {
+        return ("Scissors");
+    }
+}
+
+
+
+
+
+
+
+
+
+// let getComputerChoice = Math.floor(Math.random()*3);
+
+//     if (getComputerChoice == 0) {
+//         alert("Rock");
+//     } else if (getComputerChoice == 1) {
+//         alert("Scissors");
+//     } else if (getComputerChoice == 2) {
+//         alert("Paper");
+//     }
+    
