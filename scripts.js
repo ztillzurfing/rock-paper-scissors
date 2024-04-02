95dc3b9e72f056fff927c690d1983086b5cde5aa
ztillzurfing <94315453+ztillzurfing@users.noreply.github.com>
2024-04-02 22:55:00 +0100

    fix bugs and complete code
    moved "to.LowerCase" to one line of code
    
    set playerSelection and computerSelection within playGame()
    
    changed return("tie") to alert("tie") and added alerts for ("you won")
    and ("you lost")
    
    moved playerScore and computerScore outside of function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 
     let choice = Math.floor(Math.random()*3);
@@ -11,31 +14,70 @@ function getComputerChoice() {
     }
 }
 
+ 
+
+
 function playRound (playerSelection, computerSelection) { 
 
-    if (playerSelection.toLowerCase() === computerSelection) {
+    if (playerSelection === computerSelection) {
         return ("Tie!");
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
         return ("You won. Rock beats scissors!");
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         return ("You lost. Paper beats rock");
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
         return ("You lost. Rock beats scissors");
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         return ("You won. Scissors beats paper");
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
         return ("You won. Paper beats rock");
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
         return ("You lost. Scissors beats paper");
         
     }
     }
+
+function playGame() {
+
+
+   
+
+    for (let i = 0; i < 5; i++) {
+
+        let playerSelection = prompt("Rock, Paper or Scissors?");
+        playerSelection = playerSelection.toLowerCase();
+        
+        const computerSelection = getComputerChoice();
+
+        let result = playRound(playerSelection, computerSelection);
+        alert("The computer chose " + computerSelection); 
+        if (result.includes("won")) {
+            playerScore++;
+            alert("you won");
+        } else if (result.includes("lost")) {
+            computerScore++;
+            alert("you lost");
+        } else {
+            alert("tie")
+        }    
+            }
+       
+    if (playerScore > computerScore) {
+        alert("you win the game!");
+    } else if(computerScore > playerScore) {
+            alert("you lose the game!")
+        }
+        
+        
+    }        
+    
+playGame();
     
 
 
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+
+
 
 
 
